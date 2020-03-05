4592317eee0c09a2e4aa47d03e3d1bd78ad0bcb6
Adam Davies <adamd94@hotmail.com>
2020-03-05 22:29:30 +0000

    Add US and top economies analysis

--- src/analyzer.py
+++ src/analyzer.py
@@ -29,17 +29,26 @@ class Analyzer:
 
         self.data['current_infected_total'] = self.data['confirmed_total'] - self.data['death_total'] - self.data['recovered_total']
 
-        self.data['confirmed_change'] = self.data['confirmed_total'].diff()
-        self.data['death_change'] = self.data['death_total'].diff()
-        self.data['recovered_change'] = self.data['recovered_total'].diff()
-        self.data['current_infected_change'] = self.data['current_infected_total'].diff()
+        states = ['confirmed', 'death', 'recovered', 'current_infected']
+        self._gen_change_data(states)
+        self._gen_change_pct_data(states)
+        self._gen_change_pct_avg_data(states)
 
-        self.data['confirmed_change%'] = self.data['confirmed_total'].pct_change()
-        self.data['death_change%'] = self.data['death_total'].pct_change()
-        self.data['recovered_change%'] = self.data['recovered_total'].pct_change()
-        self.data['current_infected_change%'] = self.data['current_infected_total'].pct_change()
+        # self.data['confirmed_change%3a']
 
         self.data['lethality'] = self.data['death_total'] / self.data['recovered_total']
+
+    def _gen_change_data(self, states):
+        for state in states:
+            self.data[state+'_change'] = self.data[state+'_total'].diff()
+
+    def _gen_change_pct_data(self, states):
+        for state in states:
+            self.data[state+'_change%'] = self.data[state+'_total'].pct_change()
+
+    def _gen_change_pct_avg_data(self, states):
+        for state in states:
+            self.data[state+'_change%3a'] = self.data[state+'_change%'].rolling(window=4, min_periods=0).mean()
 
     def _clean_data(self, data):
         for country_region in self.exclude_country_regions:
@@ -59,29 +68,34 @@ class Analyzer:
     def get_dataframe(self):
         return self.data
 
-    def graph_data(self, columns=[], since_x_days_ago=0):
+    def graph_data(self, columns=[], since_x_days_ago=0, upto_x_days_ago=0):
         if len(columns) == 0:
-            self.graph_totals(since_x_days_ago)
+            self.graph_totals(since_x_days_ago, upto_x_days_ago)
         else:
             for column in columns:
-                plt.plot(self.data[column][-since_x_days_ago:], label=column)
+                plt.plot(self.data[column][-since_x_days_ago:len(self.data[column]) - upto_x_days_ago], label=column)
             plt.show()
 
-    def graph_totals(self, since_x_days_ago=0):
-        self.graph_append('_total', since_x_days_ago)
+    def graph_totals(self, since_x_days_ago=0, upto_x_days_ago=0):
+        self.graph_append('_total', since_x_days_ago, upto_x_days_ago)
 
-    def graph_changes(self, since_x_days_ago=0):
-        self.graph_append('_change', since_x_days_ago)
+    def graph_changes(self, since_x_days_ago=0, upto_x_days_ago=0):
+        self.graph_append('_change', since_x_days_ago, upto_x_days_ago)
 
-    def graph_changes_pct(self, since_x_days_ago=0):
-        self.graph_append('_change%', since_x_days_ago)
+    def graph_changes_pct(self, since_x_days_ago=0, upto_x_days_ago=0):
+        self.graph_append('_change%', since_x_days_ago, upto_x_days_ago)
 
-    def graph_append(self, append='', since_x_days_ago=0):
+    def graph_changes_pct_avg(self, since_x_days_ago=0, upto_x_days_ago=0):
+        self.graph_append('_change%3a', since_x_days_ago, upto_x_days_ago)
+
+    def graph_append(self, append='', since_x_days_ago=0, upto_x_days_ago=0):
         since_x_days_ago = -since_x_days_ago
-        plt.plot(self.data['death'+append][since_x_days_ago:], color='red', label='death')
-        plt.plot(self.data['current_infected'+append][since_x_days_ago:], color='purple', label='current infected')
-        plt.plot(self.data['confirmed'+append][since_x_days_ago:], color='orange', linestyle='dashed', label='confirmed total')
-        plt.plot(self.data['recovered'+append][since_x_days_ago:], color='green', label='recovered')
+        upto_x_days_ago = len(self.data) - upto_x_days_ago
+        plt.plot(self.data['death'+append][since_x_days_ago:upto_x_days_ago], color='red', label='death')
+        plt.plot(self.data['current_infected'+append][since_x_days_ago:upto_x_days_ago], color='purple', label='current infected')
+        plt.plot(self.data['confirmed'+append][since_x_days_ago:upto_x_days_ago], color='orange', linestyle='dashed', label='confirmed total')
+        plt.plot(self.data['recovered'+append][since_x_days_ago:upto_x_days_ago], color='green', label='recovered')
+        #plt.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
         plt.show()
 
 
