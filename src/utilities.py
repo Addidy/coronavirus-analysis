ef65e5cce62cd8e2de9696d0caa76f8f3a093983
Adam Davies <adamd94@hotmail.com>
2020-03-08 18:15:36 +0000

    add stock financial comparison

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,9 @@
 import os.path
 from git import Repo
 import pandas as pd
+import datetime as dt
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
 
 
 def fetch_virus_data():
@@ -20,3 +23,12 @@ def fetch_virus_data():
     deaths    = pd.read_csv('./datasets/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
     recovered = pd.read_csv('./datasets/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
     return confirmed, deaths, recovered
+
+
+def graph_financial_data(ticker):
+    start = dt.datetime(2020, 1, 20)
+    end = dt.datetime.now()
+
+    df = web.DataReader(ticker, 'yahoo', start, end)
+    df['Adj Close'].plot()
+    #return df
